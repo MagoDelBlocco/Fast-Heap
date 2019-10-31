d0f543242840650deb32f6aa4b1d9b36c0f44772
MagoDelBlocco <bgdnkln@gmail.com>
2019-11-01 01:51:28 +0200

    optimizations

--- Heap.h
+++ Heap.h
@@ -9,12 +9,11 @@ public:
         max_size = _size;
         storage = new _elem[max_size];
         current_bound = 1;
-        current_size = 0;
-    }  /// default c-tor
+    }
 
     heap(const heap<_elem, is_less_than>& target) {
         *this = target;
-    }  /// copy c-tor
+    }
 
     heap& operator = (const heap<_elem, is_less_than>& target) {
         delete[] storage;
@@ -22,29 +21,23 @@ public:
         storage = new _elem[max_size];
         for (unsigned i = 0; i < max_size; ++i)
             storage[i] = target.storage[i];
-        current_size = target.current_size;
         current_bound = target.current_bound;
-    }  /// copy assgn
+    }
 
-    [[gnu::hot]] void add(_elem target) {
+    [[gnu::hot]] inline void add(_elem target) {
         storage[current_bound] = target;
         sift_up(current_bound);
         ++current_bound;
-        ++current_size;
     }
 
-    [[gnu::pure]] bool empty() {
-        return (current_size == 0);
-    }
+    [[gnu::pure]] inline bool empty() { return (current_bound == 1); }
 
-    [[gnu::pure]] _elem top() {
-        return storage[1];
-    }
+    [[gnu::pure]] inline _elem top() { return storage[1]; }
 
-    [[gnu::hot]] _elem pop() {
+    [[gnu::hot]] inline _elem pop() {
         unsigned iterator = 1;
 
-        _elem retval = storage[1];
+        _elem retval = storage[iterator];
 
         while (!is_leaf(iterator)) {
             if (comparator(left_son(iterator), right_son(iterator)))
@@ -55,16 +48,7 @@ public:
                 iterator = right_son_pos(iterator);
         }
 
-        if (is_leaf(iterator) == -1) {  /// empty right leaf
-            /// this case automatically erases last elem
-            heap::swap(storage[--current_bound], storage[iterator]);
-            --current_size;
-        }
-        else {
-            heap::swap(storage[iterator], storage[current_bound - 1]);
-            --current_bound;
-            --current_size;
-        }
+        storage[iterator] = storage[--current_bound];
 
         return retval;
     }
@@ -75,26 +59,20 @@ public:
         std::cout << std::endl;
     }
 
-    ~heap() {
-        delete[] storage;
-    }
+    ~heap() { delete[] storage; }
 
 private:
     void sift_up(unsigned pos) {
         unsigned target = parent_pos(pos);
-        if (!target)
-            return;
-        if (comparator(storage[pos], storage[target])) {
-            heap::swap(storage[pos], storage[target]);
-            sift_up(target);
-        }
-    }
+        _elem aux = storage[pos];
 
-    static inline void swap(_elem& first, _elem& second) {
-        _elem auxiliary;
-        auxiliary = first;
-        first = second;
-        second = auxiliary;
+        while (target && comparator(aux, storage[target])) {
+            storage[pos] = storage[target];
+            pos = target;
+            target = parent_pos(pos);
+        }
+
+        storage[pos] = aux;
     }
 
     [[gnu::hot, gnu::pure]] inline unsigned right_son_pos(unsigned pos) {
@@ -121,20 +99,11 @@ private:
         return storage[parent_pos(pos)];
     }
 
-    [[gnu::hot]] inline int is_leaf(unsigned pos) {
-        if (right_son_pos(pos) < current_bound &&
-            left_son_pos(pos) < current_bound) {
-            return 0;
-        }
-        else if (right_son_pos(pos) == current_bound) {  // empty right_leaf
-            return -1;
-        }
-        else {
-            return 1;
-        }
+    [[gnu::hot]] inline bool is_leaf(unsigned pos) {
+        return right_son_pos(pos) < current_bound && left_son_pos(pos) < current_bound;
     }
 
     is_less_than comparator;
-    size_t max_size, current_size, current_bound;
+    size_t max_size, current_bound;
     _elem* storage;
 };
---
+++ Source.cpp
@@ -0,0 +1,9 @@
+// Copyright 2019 Ciobanu Bogdan-Calin
+// bgdnkln@gmail.com
+
+#include <iostream>
+#include "./Heap.h"
+
+int main() {
+    return 0;
+}
